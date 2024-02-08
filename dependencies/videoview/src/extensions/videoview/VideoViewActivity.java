69ee93984644442e2d38722f24a61badac2d9aa2
Lily <egzozu.be.bas@gmail.com>
2024-02-08 13:58:03 +0300

    Update VideoViewActivity.java

--- dependencies/videoview/src/extensions/videoview/VideoViewActivity.java
+++ dependencies/videoview/src/extensions/videoview/VideoViewActivity.java
@@ -9,10 +9,6 @@ import android.view.View;
 import android.widget.VideoView;
 import org.haxe.lime.HaxeObject;
 
-/**
- * author: Saw (M.A. Jigsaw)
- */
-
 public class VideoViewActivity extends Activity {
 	public static VideoView videoView;
 	public static HaxeObject callback;
@@ -39,25 +35,22 @@ public class VideoViewActivity extends Activity {
 	@Override
 	protected void onResume(){
 		super.onResume();
-		if (videoView != null) {
+		if (videoView != null)
 			videoView.resume();
-		}
 	}
 
 	@Override
 	protected void onPause(){
 		super.onPause();
-		if (videoView != null && videoView.isPlaying()) {
+		if (videoView != null && videoView.isPlaying())
 			videoView.pause();
-		}
 	}
 
 	@Override
 	protected void onDestroy(){
 		super.onDestroy();
-		if (videoView != null) {
+		if (videoView != null) 
 			videoView.stopPlayback();
-		}
 	}
 
 	@Override
@@ -67,14 +60,14 @@ public class VideoViewActivity extends Activity {
 		videoView.setVideoURI(Uri.parse(videoPath));
 		videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 			@Override
-			public void onCompletion(MediaPlayer mp) { 
+			public void onCompletion(MediaPlayer mp) {
 				callback.call("onCompletion", new Object[] {});
 				finish();
 			}
 		});
 		videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
 			@Override
-			public void onPrepared(MediaPlayer mp) { 
+			public void onPrepared(MediaPlayer mp) {
 				callback.call("onPrepared", new Object[] {});
 			}
 		});
@@ -84,9 +77,8 @@ public class VideoViewActivity extends Activity {
 	@Override
 	protected void onStop(){
 		super.onStop();
-		if (videoView != null) {
+		if (videoView != null)
 			videoView.stopPlayback();
-		}
 	}
 
 	@Override
@@ -97,9 +89,8 @@ public class VideoViewActivity extends Activity {
 
 	@Override
 	public void finish(){
-		if (videoView != null) {
+		if (videoView != null)
 			videoView.stopPlayback();
-		}
 		super.finish();
 	}
 }
