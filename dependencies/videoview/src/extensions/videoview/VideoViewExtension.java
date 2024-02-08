f50d1c2815a3643f0f5655b4ffbc194811a1c346
Lily <egzozu.be.bas@gmail.com>
2024-02-08 13:58:51 +0300

    Update VideoViewExtension.java

--- dependencies/videoview/src/extensions/videoview/VideoViewExtension.java
+++ dependencies/videoview/src/extensions/videoview/VideoViewExtension.java
@@ -5,14 +5,9 @@ import android.content.Intent;
 import org.haxe.extension.Extension;
 import org.haxe.lime.HaxeObject;
 
-/**
- * author: Saw (M.A. Jigsaw)
- */
-
 public class VideoViewExtension extends Extension {
 
 	public static final String EXTRA_VIDEOPATH = "extensions.videoviewex.EXTRA_VIDEOPATH";
-
 	public static HaxeObject callback;
 
 	public static void playVideo(String videoPath) {
@@ -20,12 +15,10 @@ public class VideoViewExtension extends Extension {
 			Intent intent = new Intent(mainActivity, VideoViewActivity.class);
 			intent.putExtra(EXTRA_VIDEOPATH, videoPath);
 			mainActivity.startActivity(intent);
-		} catch (Exception e) {
+		} catch (Exception e)
 			Log.d("VideoViewExtension", e.toString());
-		}
 	}
 
-	public static void setCallback(final HaxeObject _callback) {
+	public static void setCallback(final HaxeObject _callback)
 		callback = _callback;
-	}
 }
